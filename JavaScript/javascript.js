4023b48cea56dd666860433dbf80c39a85685226
Matheus H. Aliberto de Francisco <matheushenrique612@gmail.com>
2022-02-03 13:39:53 -0300

    versão-final-1-menu

--- JavaScript/javascript.js
+++ JavaScript/javascript.js
@@ -9,11 +9,3 @@ $(".btn-close").click(function(){
     $(".menu").fadeOut();
     $(".btn-menu").fadeIn();
 });
-$(".btn-item").click(function(){
-    $(".menu").fadeOut();
-    $(".btn-menu").fadeIn();
-});
-$(".menu").click(function(){
-    $(".menu").fadeOut();
-    $(".btn-menu").fadeIn();
-});
