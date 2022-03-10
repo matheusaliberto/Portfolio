ffb0c764abc3b15a2e100bf34e6cdbeff6a77c9c
Matheus H. Aliberto de Francisco <matheushenrique612@gmail.com>
2022-03-09 21:37:57 -0300

    adição de projeto e mudanças no html

--- JavaScript/javascript.js
+++ JavaScript/javascript.js
@@ -1,3 +1,15 @@
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true
+});
+
+// element argument can be a selector string
+//   for an individual element
+var flkty = new Flickity( '.main-carousel', {
+  // options
+});
 function whatsapp(){
     window.alert('Telefone para ligação ou whatsapp: +55 (16) 98848-1389')
 }
